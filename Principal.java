0ee1ae491fc673bef30a268ccf2da4b8d3ab1263
Maria Andrade <102066058+ofmpaz@users.noreply.github.com>
2023-07-24 09:20:59 -0300

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,25 @@
+package br.com.alura.lista;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Principal {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		List<String> lista = new ArrayList<>(); 
+
+		for (int i = 0; i < 10 ; i++) {
+			
+			new Thread(new TarefaAdicionarElemento(lista, i)).start();
+
+		}
+		
+		Thread.sleep(2000);
+
+		for (int i = 0; i < lista.size(); i++) {
+			System.out.println( i + "-" + lista.get(i));
+		}
+	}
+
+}
